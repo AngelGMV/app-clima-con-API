d6f9b9dfc2a7799a1ba79d66a33db0354967230a
Amuve <angel_ga_munoz@hotmail.com>
2025-01-29 21:33:43 -0700

    29_01_25

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,5 +1,38 @@
-export default function form() {
+import { countries } from "../../data/country";
+
+export default function Form() {
   return (
-    <div>form</div>
+    <form>
+        <div>
+            <label htmlFor="city">Ciudad: </label>
+            <input
+                id="city"
+                type="text"
+                name="city"
+                placeholder="Ingrese el nombre de la ciudad"
+            />
+        </div>
+        <div>
+            <label htmlFor="country">País: </label>
+            <select>
+            <option value="">
+                ---Seleccione un país---
+            </option>
+            {countries.map( country =>(
+                <option
+                    key={country.code}
+                    value={country.code}
+                >
+                    {country.name}
+                </option>
+            ))}
+            </select>
+        </div>
+        <input
+            type="submit"
+            value='Consultar clima'
+        />
+        
+    </form>
   )
 }

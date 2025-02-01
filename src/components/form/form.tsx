34b404a2f62fbeb52a936c3ddb88d8510e6dd078
Amuve <angel_ga_munoz@hotmail.com>
2025-02-01 11:14:26 -0700

    01_02_25

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,9 +1,30 @@
-import { countries } from "../../data/country";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { countries } from "../../data/countries";
+import { SearchType } from "../../types";
 import styles from "./Form.module.css"
 
 export default function Form() {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+
+    const handleChange = (e : ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) =>{
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = (e : FormEvent<HTMLFormElement>) =>{
+        e.preventDefault()
+    }
   return (
-    <form className={styles.form}>
+    <form 
+        className={styles.form}
+        onSubmit={handleSubmit}    
+    >
         <div className={styles.field}>
             <label htmlFor="city">Ciudad: </label>
             <input
@@ -11,11 +32,18 @@ export default function Form() {
                 type="text"
                 name="city"
                 placeholder="Ingrese el nombre de la ciudad"
+                value={search.city}
+                onChange={handleChange}
             />
         </div>
         <div className={styles.field}>
             <label htmlFor="country">País: </label>
-            <select>
+            <select
+                id="country"
+                name="country"
+                value={search.country}
+                onChange={handleChange}
+            >
             <option value="">
                 ---Seleccione un país---
             </option>
